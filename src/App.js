6c7762a35b4b0ca65918da7cfbdb8707db8c6710
davidodidi <davidodidi@yahoo.co.uk>
2022-05-13 06:09:52 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Datepicker from "./components/Datepicker";
+
+function App() {
+  return (
+    <div className="App">
+      <Datepicker label="Datepicker advanced" />
+    </div>
+  );
+}
+
+export default App;
